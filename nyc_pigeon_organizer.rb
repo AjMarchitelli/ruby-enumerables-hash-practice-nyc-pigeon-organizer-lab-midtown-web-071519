5fa744d5107275d2d5a9f14801a23d08cb683739
Anthony Joseph Marchitelli Jr <amarchitelli1993@gmail.com>
2019-07-11 22:43:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,27 +4,27 @@ def nyc_pigeon_organizer(data)
   data.each do |first, other|
     other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
+      final[name] = {:color => [], :gender => [], :lives => []}
       end 
-    end 
+  end 
   end 
   x = hash.keys
   data[:color].each do |bird_color, name|
     name.each do |bird_name|
       x.each do |item|
-        if bird_name === item
+      if bird_name === item
           hash[item][:color] << bird_color.to_s
-        end 
-      end 
+   end 
+    end 
     end 
   end 
   data[:gender].each do |gender, type|
     type.each do |bird_name|
       x.each do |item|
-        if bird_name === item
+      if bird_name === item
           hash[item][:gender] << gender.to_s
-        end 
-      end 
+   end 
+  end 
     end 
   end 
   data[:lives].each do |location, name|
